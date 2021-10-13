bf2da017987c4e297fff473af0e7f319d10d59b0
madan kumar reddy k v <madanreddy46.w@gmail.com>
2021-10-13 12:48:30 +0530

    Madan

---
+++ src/classjava/classone.java
@@ -0,0 +1,16 @@
+package classjava;
+
+public class classone {
+
+public classone() {
+	// TODO Auto-generated constructor stub
+}public static void main(String[] args) {
+	System.out.println("mddnnf");
+main(args);
+main(null);
+System.out.println();
+
+
+}
+}
+
